ffe24af7128fe0d74960c02758602f99f80e1bb9
lkmelberg <100446631+lkmelberg@users.noreply.github.com>
2022-06-15 15:45:14 +0200

    com1

---
+++ js/script.js
@@ -0,0 +1,44 @@
+// answer to question 1
+
+const cat = {
+  complain: function () {
+    console.log("Meow!");
+  },
+};
+
+// answer to question 2
+
+var heading = document.querySelector("h3");
+
+heading.innerHTML = "Updated heading";
+
+// answer to question 3
+
+heading.style.fontSize = "2em";
+
+// answer to question 4
+
+heading.classList = ".subheading";
+
+// answer to question 5
+
+var heading = document.querySelector("p");
+
+// answer to question 6
+
+// answer to question 7
+const cats = [
+  {
+    name: "Blob",
+    age: 10,
+  },
+  {
+    name: "Harold",
+  },
+  {
+    name: "Blurt",
+    age: 21,
+  },
+];
+
+// answer to question 8
